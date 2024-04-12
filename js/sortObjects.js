b2e04af428cf225f4c59fc64deef1ed7c9f402ed
Josue <ahadihjosue@gmail.com>
2024-04-12 11:02:59 +0200

    sort a list of objects

--- js/sortObjects.js
+++ js/sortObjects.js
@@ -1,3 +1,5 @@
+// You'll be passed an array of objects (list) - you must sort them in descending order based on the value of the specified property (sortBy).
+
 function sortList (sortBy, list) {
     return list.sort((a, b) => a[sortBy] - b[sortBy])
 }
