f5ea923e62af027a99239e95894bdbb24b2435bb
Josue <ahadihjosue@gmail.com>
2024-04-17 11:11:03 +0200

    digital cypher

--- js/digital-cypher-series/digitalCipher.js
+++ js/digital-cypher-series/digitalCipher.js
@@ -1,4 +1,15 @@
+/*
+MY ALGORITHM 
 
+Step 1: Convert input string (str) to array (message)
+Step 2: store letters and their unique numbers as an object (alphabetMapping)
+Step 3: Loop through message and map letters to their corresponding numbers from the alphabetMapping mapping.
+Step 4: Store the key as an array 
+Step 5: Repeat the elements of the key array until its length matches the length of "aa"
+Step 6: Add "key message" and "message" together
+Step 7: 
+
+*/
 
 const alphabetMapping = {
     'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10,
@@ -7,18 +18,28 @@ const alphabetMapping = {
 };
 
 function encode(str,  n) {
-    let arr = str.split('');
-    key = Array.from(String(n), ((n) => Number(n)))
-    console.log(key);
-    console.log(arr)
-    let encodedArr = [];
-    
-    for (i=0; i<arr.length; i++) {
-      encodedArr.push(alphabetMapping[arr[i]]);
-    }
-    console.log(encodedArr);
+  let message = str.split('');
+  key = Array.from(String(n), ((n) => Number(n)))
+  let extendedKey = [];
+  let messageAsInt = [];
+  
+  for (let i=0; i<message.length; i++) {
+    messageAsInt.push(alphabetMapping[message[i]]);
   }
+  
+  while(extendedKey.length < message.length) {
+    for (let i = 0; i < key.length && extendedKey.length < message.length; i++) {
+      extendedKey.push(key[i])
+    }
+    
+  }
+  
+  console.log(extendedKey);
+  
+  let encodedMessage = messageAsInt.map((element, index) => element+extendedKey[index]);
+  
+  return encodedMessage;
+  
+}
 
-
-
-encode("scout", 1939)
+encode("innerpeace", 1939)
