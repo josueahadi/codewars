3d2732207ed42cbe1d8de7baf836a12be75f1da2
Josue <ahadihjosue@gmail.com>
2024-04-29 17:15:26 +0200

    modified solution

--- js/capitalizeFirstLetter.js
+++ js/capitalizeFirstLetter.js
@@ -1,6 +1,5 @@
 const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 
 for (let day of days) {
-    let firstLetter = day.charAt(0).toUpperCase();
-    console.log(`${firstLetter}${day.slice(1)}`);
+    console.log(day.charAt(0).toUpperCase() + day.slice(1));   
 }
